4766488381fd026117b9c3f59522e6d2dac9b8e1
edovvv <edovvv@gmail.com>
2021-10-13 10:45:02 +0300

    FirstScreen_mvvm

--- Movie/Movie/MainModule/View/ViewController.swift
+++ Movie/Movie/MainModule/View/ViewController.swift
@@ -7,20 +7,34 @@ import UIKit
 final class ViewController: UIViewController {
     // MARK: - Private Properties
 
+    var viewModel: MoviesViewModelProtocol!
+
     private var myTableView = UITableView()
     private let identifire = "MyCell"
     private var id: Int?
     private var titleLabel: String?
     private var imageString: String?
     private var overviewLabel: String?
-    var result: Movies?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Movie"
         createTableView()
-        fetchData()
+        updateView()
+    }
+
+    func updateView() {
+        viewModel.fetchData()
+        viewModel.updateViewData = { [weak self] in
+            DispatchQueue.main.async {
+                self?.myTableView.reloadData()
+            }
+        }
+    }
+
+    func installViewModel(viewModel: MoviesViewModelProtocol) {
+        self.viewModel = viewModel
     }
 
     // MARK: - create Private Medoth
@@ -33,28 +47,6 @@ final class ViewController: UIViewController {
         myTableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(myTableView)
     }
-
-    // MARK: - create medoth
-
-    func fetchData() {
-        let jsonUrlString =
-            "https://api.themoviedb.org/3/movie/popular?api_key=1ee34276a75d38c0cae118c698cdcfdf&language=ru-RU"
-        guard let url = URL(string: jsonUrlString) else { return }
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            guard let data = data else { return }
-
-            do {
-                let decoder = JSONDecoder()
-                self.result = try decoder.decode(Movies.self, from: data)
-                DispatchQueue.main.async {
-                    self.myTableView.reloadData()
-                }
-            } catch {
-                print("Error")
-            }
-
-        }.resume()
-    }
 }
 
 extension ViewController: UITableViewDelegate {
@@ -64,20 +56,20 @@ extension ViewController: UITableViewDelegate {
 
     func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
         let newVC = NewViewController()
-        newVC.id = result?.results[indexPath.row].id
+        newVC.id = viewModel.results?[indexPath.row].id
         navigationController?.pushViewController(newVC, animated: true)
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
-        result?.results.count ?? 0
+        viewModel.results?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: identifire) as? MyTableViewCell
         else { return UITableViewCell() }
-        guard let movie = result?.results[indexPath.row] else { return UITableViewCell() }
+        guard let movie = viewModel.results?[indexPath.row] else { return UITableViewCell() }
         cell.configur(movie: movie)
         return cell
     }
