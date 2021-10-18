71ad550715d74677f20f4b516e195354aed13a60
edovvv <edovvv@gmail.com>
2021-10-18 10:38:23 +0300

    add

--- Movie/MovieTests/RepositoryTest.swift
+++ Movie/MovieTests/RepositoryTest.swift
@@ -26,28 +26,3 @@ final class RepositoryTest: XCTestCase {
         XCTAssertEqual(testArray.first?.mockName, result?.first?.mockName)
     }
 }
-
-// final class RepositoryTest: XCTestCase {
-//    private var repository: RealmRepository<MockModel>!
-//    private var testVar = MockModel()
-//
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-// }
